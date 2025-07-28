ff8285b9f94164d6c420e2163d7fa25cd47a8413
chantelj <chanteljohnson06@gmail.com>
2025-07-28 16:46:37 -0500

    Initial Commit

---
+++ CoPlanr/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CoPlanr
+//
+//  Created by Chantel Johnson on 7/28/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
